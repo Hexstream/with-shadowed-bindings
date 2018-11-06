ae2f96429a9695ecc2ffd9832a055d8244e5836b
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-11-06 16:30:26 -0500

    Simplify by treating setf functions as just a subcase of functions.

--- main.lisp
+++ main.lisp
@@ -6,49 +6,41 @@
     (symbol (values :variable binding))
     ((cons (eql function) (cons t null))
      (let ((name (second binding)))
-       (etypecase name
-         (symbol (values (if (macro-function name env)
+       (values (etypecase name
+                 (symbol (if (macro-function name env)
                              :macro
-                             :function)
-                         name))
-         ((cons (eql setf) (cons symbol null))
-          (values :setf-function (second name))))))))
+                             :function))
+                 ((cons (eql setf) (cons symbol null))
+                  :function))
+               name)))))
 
 (defun %invalid-access (kind name)
   (error "Can't access shadowed ~A ~S."
          (ecase kind
            (:variable "variable")
            (:macro "macro")
-           ((:function :setf-function) "function"))
+           (:function "function"))
          name))
 
 (defun %add-shadowing (body kind name)
-  (let ((invalid-access `(%invalid-access ',kind
-                                          ',(if (eq kind :setf-function)
-                                                `(setf ,name)
-                                                name))))
+  (let ((invalid-access `(%invalid-access ',kind ',name)))
     (ecase kind
-      (:variable
-       (let ((macro-name (gensym (symbol-name name))))
-         `(macrolet ((,macro-name ()
-                       ,invalid-access))
-            (symbol-macrolet ((,name (,macro-name)))
-              (declare (ignorable ,name))
-              ,@body))))
+      (:variable (let ((macro-name (gensym (symbol-name name))))
+                   `(macrolet ((,macro-name ()
+                                 ,invalid-access))
+                      (symbol-macrolet ((,name (,macro-name)))
+                        (declare (ignorable ,name))
+                        ,@body))))
       (:macro `(macrolet ((,name (&rest rest)
                             (declare (ignore rest))
                             ,invalid-access))
                  (declare (ignorable ,name))
                  ,@body))
-      ((:function :setf-function)
-       (let ((name (ecase kind
-                     (:function name)
-                     (:setf-function `(setf ,name)))))
-         `(flet ((,name (&rest rest)
-                   (declare (ignore rest))
-                   ,invalid-access))
-            (declare (ignorable #',name))
-            ,@body))))))
+      (:function `(flet ((,name (&rest rest)
+                           (declare (ignore rest))
+                           ,invalid-access))
+                    (declare (ignorable #',name))
+                    ,@body)))))
 
 (defmacro with-shadowed-bindings (bindings &body body &environment env)
   (if bindings
